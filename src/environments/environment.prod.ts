77656c2b9feacae6e96c6b3f1b31cd5988252668
Thushanthan <mohanthushanthan2@gmail.com>
2021-05-31 04:20:31 -0600

    project files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig:{
+  apiKey: "AIzaSyAM-0ZUcH0f1nabgDiE5UU6L6d42CDsSVo",
+  authDomain: "mobilemarketplace-70902.firebaseapp.com",
+  projectId: "mobilemarketplace-70902",
+  storageBucket: "mobilemarketplace-70902.appspot.com",
+  messagingSenderId: "140873318400",
+  appId: "1:140873318400:web:84b6c22b8b9483dae618e5"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseconfig:{
+  apiKey: "AIzaSyAM-0ZUcH0f1nabgDiE5UU6L6d42CDsSVo",
+  authDomain: "mobilemarketplace-70902.firebaseapp.com",
+  projectId: "mobilemarketplace-70902",
+  storageBucket: "mobilemarketplace-70902.appspot.com",
+  messagingSenderId: "140873318400",
+  appId: "1:140873318400:web:84b6c22b8b9483dae618e5"
+  } 
 };
 
 /*
